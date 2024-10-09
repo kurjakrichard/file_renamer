863762ed96a02f3abadd37b6a818781a0a052bca
Richard Kurjak <kurjak.richard@gmail.com>
2024-10-10 00:39:47 +0200

    No need excel file edit

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -116,13 +116,15 @@ class _HomePageState extends State<HomePage> {
                     /// file might be picked
                     setState(() {
                       if (pickedFile != null) {
-                        fileName = pickedFile.files.single.name;
+                       fileName = pickedFile.files.single.name;
                         var bytes = File(pickedFile.files.first.path!)
                             .readAsBytesSync();
                         var excel = Excel.decodeBytes(bytes);
                         for (var table in excel.tables.keys) {
                           for (var row in excel.tables[table]!.rows) {
-                            excelList['${row[1]!.value}'] = '${row[0]!.value}';
+                            //print('${row[9]!.value}');
+                            //print('${row[1]!.value}'.substring(3));
+                            excelList['${row[9]!.value}'] = '${row[1]!.value}'.substring(3);
                           }
                         }
                       }
@@ -177,12 +179,12 @@ class _HomePageState extends State<HomePage> {
     if (excelList.isNotEmpty && files.isNotEmpty) {
       for (var file in files) {
         String oldName = p.basename(file.path);
-        print('Oldnme $oldName');  
+       // print('Oldname $oldName');  
         String path = p.dirname(file.path);
         String addString = excelList[oldName.split('_')[0]] ??
             excelList[oldName.split(' ')[0]] ??
             '';
-            print('addString $addString');
+          //  print('addString $addString');
         if (addString != '') {
           String newName = '${addString.replaceAll('/', '_')}_$oldName';
           fileList[oldName] = newName;
